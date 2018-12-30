63a76d753815ff244fccda76051830067068168a
singcl <24661881@qq.com>
2018-12-30 21:29:11 +0800

    :art: Adjust the Project constructure

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@
 
 import React, { Component } from 'react'
 import { Platform, StyleSheet, Text, View } from 'react-native'
-import HelloWorld from './src/components/HelloWorld'
+import HelloWorld from './components/HelloWorld'
 
 const instructions = Platform.select({
     ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -1,19 +1,13 @@
 //@flow
 
-import React, { Component } from 'react'
+import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
-type Props = {}
-
-export default class HelloWorld extends Component<Props> {
-    render() {
-        return (
-            <View>
-                <Text style={styles.hello}>Hello World</Text>
-            </View>
-        )
-    }
-}
+const HelloWorld = () => (
+    <View>
+        <Text style={styles.hello}>Hello World</Text>
+    </View>
+)
 
 const styles = StyleSheet.create({
     hello: {
@@ -22,3 +16,5 @@ const styles = StyleSheet.create({
         color: '#a3a1c4',
     },
 })
+
+export default HelloWorld

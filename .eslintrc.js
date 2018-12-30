8680a55dcabf7591814987b695a0c88cd8e42a23
singcl <24661881@qq.com>
2018-12-30 19:02:30 +0800

    :wrench: update config.

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,8 @@ module.exports = {
         es6: true,
         node: true,
     },
-    extends: 'eslint:recommended',
+    parser: 'babel-eslint',
+    extends: ['eslint:recommended', 'plugin:flowtype/recommended', 'plugin:react/recommended'],
     parserOptions: {
         ecmaFeatures: {
             jsx: true,
@@ -13,10 +14,31 @@ module.exports = {
         ecmaVersion: 2018,
         sourceType: 'module',
     },
-    plugins: ['react'],
+
+    settings: {
+        flowtype: {
+            onlyFilesWithFlowAnnotation: true, //只在添加@flow注释的文件才做检查
+        },
+        react: {
+            createClass: 'createReactClass', // Regex for Component Factory to use,
+            // default to "createReactClass"
+            pragma: 'React', // Pragma to use, default to "React"
+            // version: '16.0', // React version, default to the latest React stable release
+            flowVersion: '0.79', // Flow version
+        },
+        propWrapperFunctions: [
+            // The names of any function used to wrap propTypes, e.g. `forbidExtraProps`. If this isn't set, any propTypes wrapped in a function will be skipped.
+            'forbidExtraProps',
+            { property: 'freeze', object: 'Object' },
+            { property: 'myFavoriteWrapper' },
+        ],
+    },
+
+    plugins: ['react', 'flowtype'],
     rules: {
         indent: ['error', 4],
         quotes: ['error', 'single'],
         semi: ['error', 'never'],
+        'max-len': ['error', { code: 200, tabWidth: 4, comments: 65 }],
     },
 }
